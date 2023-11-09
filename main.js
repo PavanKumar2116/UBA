328e5e572e6e1ba3baec6da60157d4478e6e896d
PavanKumar2116 <149879002+PavanKumar2116@users.noreply.github.com>
2023-11-09 12:31:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,66 @@
-const zoomImage = document.getElementById('zoomImage');
+const zoomImage = document.getElementById('zoomImage1');
 
-zoomImage.addEventListener('click', function() {
-  if (zoomImage.style.transform === 'scale(1.5)') {
-    zoomImage.style.transform = 'scale(1)';
-    zoomImage.style.cursor = 'zoom-in';
+zoomImage1.addEventListener('click', function() {
+  if (zoomImage1.style.transform === 'scale(1.5)') {
+    zoomImage1.style.transform = 'scale(1)';
+    zoomImage1.style.cursor = 'zoom-in';
   } else {
-    zoomImage.style.transform = 'scale(1.5)';
-    zoomImage.style.cursor = 'zoom-out';
+    zoomImage1.style.transform = 'scale(1.5)';
+    zoomImage1.style.cursor = 'zoom-out';
   }
 })
-const zoomImage2 = document.getElementById('zoomImage');
+const zoomImage2 = document.getElementById('zoomImage2');
 
-zoomImage.addEventListener('click', function() {
-  if (zoomImage.style.transform === 'scale(1.5)') {
-    zoomImage.style.transform = 'scale(1)';
-    zoomImage.style.cursor = 'zoom-in';
+zoomImage2.addEventListener('click', function() {
+  if (zoomImage2.style.transform === 'scale(1.5)') {
+    zoomImage2.style.transform = 'scale(1)';
+    zoomImage2.style.cursor = 'zoom-in';
   } else {
-    zoomImage.style.transform = 'scale(1.5)';
-    zoomImage.style.cursor = 'zoom-out';
+    zoomImage2.style.transform = 'scale(1.5)';
+    zoomImage2.style.cursor = 'zoom-out';
+  }
+})
+const zoomImage3 = document.getElementById('zoomImage3');
+
+zoomImage3.addEventListener('click', function() {
+  if (zoomImage3.style.transform === 'scale(1.5)') {
+    zoomImage3.style.transform = 'scale(1)';
+    zoomImage3.style.cursor = 'zoom-in';
+  } else {
+    zoomImage3.style.transform = 'scale(1.5)';
+    zoomImage3.style.cursor = 'zoom-out';
+  }
+})
+const zoomImage4 = document.getElementById('zoomImage4');
+
+zoomImage4.addEventListener('click', function() {
+  if (zoomImage4.style.transform === 'scale(1.5)') {
+    zoomImage4.style.transform = 'scale(1)';
+    zoomImage4.style.cursor = 'zoom-in';
+  } else {
+    zoomImage4.style.transform = 'scale(1.5)';
+    zoomImage4.style.cursor = 'zoom-out';
+  }
+})
+const zoomImage5 = document.getElementById('zoomImage5');
+
+zoomImage5.addEventListener('click', function() {
+  if (zoomImage5.style.transform === 'scale(1.5)') {
+    zoomImage5.style.transform = 'scale(1)';
+    zoomImage5.style.cursor = 'zoom-in';
+  } else {
+    zoomImage5.style.transform = 'scale(1.5)';
+    zoomImage5.style.cursor = 'zoom-out';
+  }
+})
+const zoomImage6 = document.getElementById('zoomImage6');
+
+zoomImage6.addEventListener('click', function() {
+  if (zoomImage6.style.transform === 'scale(1.5)') {
+    zoomImage6.style.transform = 'scale(1)';
+    zoomImage6.style.cursor = 'zoom-in';
+  } else {
+    zoomImage6.style.transform = 'scale(1.5)';
+    zoomImage6.style.cursor = 'zoom-out';
   }
 })
