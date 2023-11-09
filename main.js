63f82cd78737682ad973df196860b7f8707aeabe
PavanKumar2116 <149879002+PavanKumar2116@users.noreply.github.com>
2023-11-09 12:29:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ zoomImage.addEventListener('click', function() {
     zoomImage.style.cursor = 'zoom-out';
   }
 })
+const zoomImage2 = document.getElementById('zoomImage');
+
+zoomImage.addEventListener('click', function() {
+  if (zoomImage.style.transform === 'scale(1.5)') {
+    zoomImage.style.transform = 'scale(1)';
+    zoomImage.style.cursor = 'zoom-in';
+  } else {
+    zoomImage.style.transform = 'scale(1.5)';
+    zoomImage.style.cursor = 'zoom-out';
+  }
+})
